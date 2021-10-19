3c0fbbd3643c891779e4029102d693d8f07ff2f0
experiencei <ayelojahighbee01@gmail.com>
2021-10-19 10:23:53 +0100

    using materials icon and core to prop the header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/header/Header';
 import './App.css';
+import TinderCards from './components/tindercard/TinderCards';
 
 function App() {
   return (
     <div className="app">
-       <Header>guy let build the tinder</Header>
+       <Header/>
+       <TinderCards/>
     </div>
   );
 }
---
+++ src/components/tindercard/TinderCards.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TinderCards() {
+    return (
+        <div className="tinderCard">
+            
+        </div>
+    )
+}
+
+export default TinderCards
