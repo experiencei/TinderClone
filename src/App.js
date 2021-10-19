060dc87acd41ff7a84fdb0a0584ddd6b5e654a3c
experiencei <ayelojahighbee01@gmail.com>
2021-10-19 11:02:32 +0100

    implementing the Swipe functionality

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from './components/header/Header';
 import './App.css';
 import TinderCards from './components/tindercard/TinderCards';
+import SwipeButtons from './components/swipebutton/SwipeButtons';
 
 function App() {
   return (
     <div className="app">
        <Header/>
        <TinderCards/>
+       <SwipeButtons/>
     </div>
   );
 }
---
+++ src/components/swipebutton/SwipeButtons.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./SwipeButtons.css"
+
+
+function SwipeButtons() {
+    return (
+        <div className="SwipeButtons">
+            
+        </div>
+    )
+}
+
+export default SwipeButtons
