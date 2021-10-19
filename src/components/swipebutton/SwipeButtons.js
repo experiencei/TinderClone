d8015b94af5808aba8ca30dbab8c480c6ca516eb
experiencei <ayelojahighbee01@gmail.com>
2021-10-19 11:10:44 +0100

    implementing the Swipe functionality

--- src/components/swipebutton/SwipeButtons.js
+++ src/components/swipebutton/SwipeButtons.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import "./SwipeButtons.css"
+import  IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/core/Close";
+import { StarRate } from '@material-ui/icons';
+import { Favorite } from '@material-ui/icons';
+import { FlashOn } from '@material-ui/icons';
+import { Replay }from '@material-ui/icons'
+
 
 
 function SwipeButtons() {
     return (
-        <div className="SwipeButtons">
+        <div className="swipeButtons">
             
         </div>
     )
