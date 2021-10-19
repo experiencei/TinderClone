6d4efea71a2662f4974a61cedc0f47d0273c43b2
experiencei <ayelojahighbee01@gmail.com>
2021-10-19 10:39:21 +0100

    working and implementing the use of tinder reactz

--- src/components/tindercard/TinderCards.js
+++ src/components/tindercard/TinderCards.js
@@ -1,21 +1,40 @@
 import React,{ useState}from 'react'
+import TinderCard from 'react-tinder-card';
 import "./TinderCards.css";
 
 function TinderCards() {
      const [people , setPeople] = useState([{
          name:"Elon Musk",
          url : ""
-     }] )
+     },
+     {
+        name:"Elon Musk",
+        url : ""
+    }] )
 
+  const swiped = (direction , nameToDelete) => { 
+    //   setLastDirection(direction)
+    }
 
+    const outOfFrame = (name) => {
+
+    }
 
     return (
         <div className="tinderCards">
-             {
-                 people.map(person => person(
-                     <h1>{person.name}</h1>
+            <div className="tinderCards__cardContainer">
+            {
+                 people.map(person =>(
+                     <TinderCard
+                       className="swipe"
+                       key={person.name}
+                       preventSwipe={"up" , "down"}
+                       onSwipe={(dir) => swiped(dir , person.name)}
+                       onCardLeftScreen={() => outOfFrame(person.name)}
+                     >{person.name}</TinderCard>
                  ))
              }
+            </div>
         </div>
     )
 }
