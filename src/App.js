5389640f7f76325c43d2aa63ecdd643254343ba2
experiencei <ayelojahighbee01@gmail.com>
2021-10-19 10:16:58 +0100

    using materials icon and core to prop the header

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './components/header/Header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+       <Header>guy let build the tinder</Header>
+    </div>
+  );
+}
+
+export default App;
